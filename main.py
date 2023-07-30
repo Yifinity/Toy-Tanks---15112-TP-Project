faaff1fa025fd6e76c42f76409545f2fc69e1b05
Yifinity <yifan99.jiang@gmail.com>
2023-07-30 14:14:19 -0400

    empty project creation

---
+++ main.py
@@ -0,0 +1,27 @@
+from cmu_graphics import *
+
+''' 
+* Toy Tanks
+* CMU 15112 Term Project
+* Author: Yifan Jiang
+* Date: 10 August 2023
+*
+'''
+
+
+def onAppStart(app):
+    pass
+
+def onMousePress(app, mouseX, mouseY):
+    pass
+
+def onKeyPress(app, key):
+    pass
+
+def redrawAll(app):
+    pass
+
+def main():
+    runApp()
+
+main()
