b7ca9b1e422f7ebf0e0f2303d32ef447c64cebdb
Yifinity <yifan99.jiang@gmail.com>
2023-08-05 14:03:35 -0400

    create ProjectileManager class

---
+++ ProjectileManager.py
@@ -0,0 +1,6 @@
+# Class the manages all the projectiles that are in the game
+# Should have permissions to remove projectiles, and call kill functions
+class ProjectileManager:
+    def __init__(self):
+        
+
