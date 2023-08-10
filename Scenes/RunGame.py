87173076131ce79d05b9817801589b60137aea80
Yifinity <yifan99.jiang@gmail.com>
2023-08-09 20:03:09 -0400

    Have all starting tanks be white by default

--- Scenes/RunGame.py
+++ Scenes/RunGame.py
@@ -51,12 +51,12 @@ class RunGame:
         ]
         # Add enemies at appropiate locations. 
         app.objects.append(Enemy(app.enemyCoords[0][0], app.enemyCoords[0][1]))
-        app.objects.append(GreenEnemy(app.enemyCoords[1][0], app.enemyCoords[1][1]))
-        app.objects.append(RedEnemy(app.enemyCoords[2][0], app.enemyCoords[2][1]))
+        app.objects.append(Enemy(app.enemyCoords[1][0], app.enemyCoords[1][1]))
+        app.objects.append(Enemy(app.enemyCoords[2][0], app.enemyCoords[2][1]))
 
-        app.objects.append(YellowEnemy(app.enemyCoords[3][0], app.enemyCoords[3][1]))
-        app.objects.append(GreenEnemy(app.enemyCoords[4][0], app.enemyCoords[4][1]))
-        app.objects.append(RedEnemy(app.enemyCoords[5][0], app.enemyCoords[5][1]))
+        app.objects.append(Enemy(app.enemyCoords[3][0], app.enemyCoords[3][1]))
+        app.objects.append(Enemy(app.enemyCoords[4][0], app.enemyCoords[4][1]))
+        app.objects.append(Enemy(app.enemyCoords[5][0], app.enemyCoords[5][1]))
 
 
     def onStep(self):
